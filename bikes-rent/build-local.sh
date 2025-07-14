dfae4cad0e54b2b44338ca1096ab84c072484d86
VMila <vi-milanez@hotmail.com>
2025-07-14 15:09:36 -0300

    mudando arquivo de build

--- bikes-rent/build-local.sh
+++ bikes-rent/build-local.sh
@@ -1,21 +1,42 @@
 #!/bin/bash
-
-# Este script constrói a imagem da aplicação e a carrega no Docker do Minikube.
-
-# Garante que o script pare se algum comando falhar
 set -e
 
-echo ">>> 1. Apontando o terminal para o ambiente Docker do Minikube..."
+echo ">>> Verificando o status do Minikube..."
+if minikube status | grep -q "host: Running"; then
+    echo ">>> Minikube já está em execução."
+else
+    echo ">>> Minikube não está rodando. Iniciando K8s..."
+    minikube start
+fi
+
+echo ">>> Apontando o terminal para o ambiente Docker do Minikube..."
 eval $(minikube -p minikube docker-env)
 
-echo ">>> 2. Construindo a imagem da aplicação 'bike-app'..."
-# O nome da imagem 'bike-app' deve ser o mesmo que você usará no seu Helm Chart.
-# O '.' no final indica que o Dockerfile está no diretório atual.
+echo ">>> Construindo a imagem da aplicação 'bike-app'..."
 docker build -t bike-app:latest .
 
-echo ">>> 3. Verificando as imagens disponíveis no Minikube..."
-# Lista as imagens para confirmar que 'bike-app' foi carregada com sucesso.
+echo ">>> Verificando as imagens disponíveis no Minikube..."
 docker images | grep bike-app
 
 echo ""
-echo ">>> Processo concluído! A imagem 'bike-app:latest' está pronta para ser usada pelo seu cluster Kubernetes."
+echo ">>> Processo concluído! A imagem 'bike-app:latest' está pronta para ser usada pelo K8s."
+
+cd bikes-chart
+
+echo ">>> Habilitando ingress."
+minikube addons enable ingress
+
+echo ">>> Esperando o Ingress Controller ficar pronto (pode demorar um pouco)..."
+kubectl wait --namespace ingress-nginx \
+  --for=condition=ready pod \
+  --selector=app.kubernetes.io/component=controller \
+  --timeout=120s
+
+echo ">>> Atualizando dependências do chart."
+helm dependency update
+
+echo ">>> Instalando bike-release."
+helm upgrade bike-release . --install
+
+echo ""
+echo ">>> Build concluído!"
